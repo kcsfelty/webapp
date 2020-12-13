50d4798152c837b13549f7cfd449c98c3ca1a416
Ricky Felty <kcsfelty@gmail.com>
2020-12-13 03:12:14 -0600

    make timestampAge display iOS friendly

--- src/WeatherApp/Location/Location.js
+++ src/WeatherApp/Location/Location.js
@@ -23,11 +23,17 @@ export const Location = (props) => {
     return (
         <LocationStyle>
             <Highlight size={25}>{`${name}, ${region}`}</Highlight>
-            <div>{labelTime((new Date(last_updated.replace(" ", "T")))/ 1000)} {readableDate(last_updated.substring(0,10))}</div>
+            <div>{labelTime((new Date())/ 1000)} {readableDate(localtime.substring(0,10))}</div>
             <div>{`Latitude: ${lat}°, Longitude: ${lon}°`}</div>
             <div>{`Timezone ${tz_id.replace("_", " ")}`}</div>
-            <div>{`Last updated ${Math.floor((Date.parse(localtime.replace(" ", "T")) - Date.parse(last_updated.replace(" ", "T")))/1000/60)} minutes ago`}</div>
-            <div>{`Last fetched ${Math.floor((Date.parse(fetchTime) - Date.parse(fetchTime))/1000/60)} minutes ago`}</div>
+            <div>{`Last updated ${timestampAgeMinutes(last_updated)} minute${timestampAgeMinutes(last_updated) === 1 ? "" : "s"} ago`}</div>
+            <div>{`Data is ${timestampAgeMinutes(fetchTime)} minute${timestampAgeMinutes(fetchTime) === 1 ? "" : "s"} old`}</div>
         </LocationStyle>
     )
+}
+
+const timestampAgeMinutes = (then) => {
+    const iosFriendly = isNaN(Date.parse(then)) ? Date.parse(then.replace(" ", "T")) : Date.parse(then)
+
+    return Math.floor((Date.parse(new Date()) - iosFriendly)/60/1000)
 }
